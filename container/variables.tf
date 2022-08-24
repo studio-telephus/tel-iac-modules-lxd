9e4caa0d18bb843a820cb2ec4ffebfe29df61ffa
Kristo Aun <@checkout.com>
2022-08-24 18:49:10 +0300

    LXD container

---
+++ container/variables.tf
@@ -0,0 +1,49 @@
+variable "name" {
+  type = string
+}
+
+variable "image" {
+  type    = string
+  default = "images:debian/buster"
+}
+
+variable "profiles" {
+  type    = list(string)
+  default = []
+}
+
+variable "nic" {
+  type = object({
+    name       = string
+    properties = map(string)
+  })
+}
+
+variable "volumes" {
+  type = list(object({
+    pool        = string
+    volume_name = string
+    path        = string
+  }))
+  default = []
+}
+
+variable "autostart" {
+  type    = bool
+  default = false
+}
+
+variable "files" {
+  type = object({
+    target = string
+    list  = list(string)
+  })
+}
+
+variable "exec" {
+  type = object({
+    entrypoint = string
+    environment  = map(any)
+  })
+}
+
